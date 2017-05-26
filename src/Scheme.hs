2af109f2e8945c6ddce3c5f204a3134843449040
LambdaXymox <lambda.xymox@gmail.com>
2017-05-26 21:42:16 +0000

    Add Scheme module for testing interpreter components.

---
+++ src/Scheme.hs
@@ -0,0 +1,13 @@
+module Scheme
+    (
+        module Scheme.Types,
+        module Scheme.Parser,
+        module Scheme.Env,
+        module Scheme.Eval,
+    )
+    where
+
+import Scheme.Types 
+import Scheme.Parser
+import Scheme.Env
+import Scheme.Eval
