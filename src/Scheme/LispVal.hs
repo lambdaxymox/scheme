631d5a1652753123b3e2da9cc280377b030fa059
LambdaXymox <lambda.xymox@gmail.com>
2017-05-26 06:44:04 +0000

    Refactor LispVal type into a separate module.

---
+++ src/Scheme/LispVal.hs
@@ -0,0 +1,24 @@
+module Scheme.LispVal
+    (
+        LispVal(..)
+    )
+    where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+instance Show LispVal where
+    show (String contents) = "\"" ++ contents ++ "\""
+    show (Atom name) = name
+    show (Number contents) = show contents
+    show (Bool True) = "#t"
+    show (Bool False) = "#f"
+    show (List contents) = "(" ++ unwordsList contents ++ ")"
+    show (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ show tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map show
--- src/Scheme/Parser.hs
+++ src/Scheme/Parser.hs
@@ -9,27 +9,8 @@ import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
 import Numeric
+import Scheme.LispVal
 
-
-data LispVal = Atom String
-             | List [LispVal]
-             | DottedList [LispVal] LispVal
-             | Number Integer
-             | String String
-             | Bool Bool
-
-instance Show LispVal where
-    show (String contents) = "\"" ++ contents ++ "\""
-    show (Atom name) = name
-    show (Number contents) = show contents
-    show (Bool True) = "#t"
-    show (Bool False) = "#f"
-    show (List contents) = "(" ++ unwordsList contents ++ ")"
-    show (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ show tail ++ ")"
-
-
-unwordsList :: [LispVal] -> String
-unwordsList = unwords . map show
 
 escapedChars :: String
 escapedChars = "\"\n\r\t\\\'"
