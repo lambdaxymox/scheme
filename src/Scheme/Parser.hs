5cde101d95abc5eb54227bb3af19fdbd54cfd13a
LambdaXymox <lambda.xymox@gmail.com>
2017-05-26 05:44:04 +0000

    Add support for numbers in multiple bases per the R5RS specification.

--- src/Scheme/Parser.hs
+++ src/Scheme/Parser.hs
@@ -7,6 +7,7 @@ module Scheme.Parser
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
+import Numeric
 
 
 data LispVal = Atom String
@@ -22,8 +23,8 @@ escapedChars = "\"\n\r\t\\\'"
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
-spaces :: Parser ()
-spaces = skipMany1 space
+skipSpaces :: Parser ()
+skipSpaces = skipMany1 space
 
 nonEscapedChar :: Parser Char
 nonEscapedChar = noneOf escapedChars
@@ -40,12 +41,17 @@ parseExpr :: Parser LispVal
 parseExpr =  parseAtom
          <|> parseString
          <|> parseNumber
+         <|> parseQuoted
+         <|> do char '('
+                x <- try parseList <|> parseDottedList
+                char ')'
+                return x
 
 parseString :: Parser LispVal
 parseString = do
-    _ <- char '"'
-    x <- many $ escapedChar <|> nonEscapedChar
-    _ <- char '"'
+    char '"'
+    x <- many (escapedChar <|> nonEscapedChar)
+    char '"'
     return $ String x
 
 parseAtom :: Parser LispVal
@@ -59,4 +65,42 @@ parseAtom = do
         _    -> Atom atom
 
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = do
+    prefix <- numberPrefix <|> string ""
+    digits <- many1 digit
+    return $ case prefix of
+        ""   -> Number . read $ digits
+        "#b" -> Number . fst . head . readBin $ digits
+        "#o" -> Number . fst . head . readOct $ digits
+        "#d" -> Number . read $ digits
+        "#x" -> Number . fst . head . readHex $ digits
+
+numberPrefix :: Parser String
+numberPrefix = char '#' >>= \ch1 -> oneOf "bodx" >>= \ch2 -> return [ch1,ch2]
+
+readBin :: ReadS Integer
+readBin = readInt 2 isBinaryDigit binConvert
+    where
+        isBinaryDigit :: Char -> Bool
+        isBinaryDigit ch = ch `elem` "01"
+
+        binConvert :: Char -> Int
+        binConvert '0' = 0
+        binConvert '1' = 1
+        binConvert  _  = error "readBin failed."
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr skipSpaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+    lhead <- endBy parseExpr skipSpaces
+    ltail <- char '.' >> skipSpaces >> parseExpr
+    return $ DottedList lhead ltail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+    char '\''
+    x <- parseExpr
+    return $ List [Atom "quote", x]
+
