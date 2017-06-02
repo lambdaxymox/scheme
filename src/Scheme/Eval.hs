54b4fd19a736199771f9977e6e927df3ae042088
LambdaXymox <lambda.xymox@gmail.com>
2017-06-02 07:44:59 +0000

    Replace type signature on readExpr and readExprList functions in evaluator.

--- src/Scheme/Eval.hs
+++ src/Scheme/Eval.hs
@@ -254,10 +254,10 @@ readOrThrow parser input = case parse parser "lisp" input of
     Left err  -> throwError $ Parser err
     Right val -> return val
 
---readExpr :: String -> ThrowsError a
+readExpr :: String -> ThrowsError LispVal
 readExpr = readOrThrow parseExpr
 
---readExprList :: String -> ThrowsError a
+readExprList :: String -> ThrowsError LispVal
 readExprList = readOrThrow (endBy parseExpr spaces)
 
 evalString :: Env -> String -> IO String
